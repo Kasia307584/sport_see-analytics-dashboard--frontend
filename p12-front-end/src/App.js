d25569820305c36a09b0d475485519be04ea1991
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2023-06-23 11:53:11 +0200

    project initialisation

--- p12-front-end/src/App.js
+++ p12-front-end/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import * as React from "react";
+import { Routes, Route } from "react-router-dom";
+import PagesLayout from "./PagesLayout";
+// import User from "./User";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<PagesLayout />}>
+        {/* <Route path=":userId" element={<User />} /> */}
+      </Route>
+    </Routes>
   );
 }
-
-export default App;
---
+++ p12-front-end/src/PagesLayout.js
@@ -0,0 +1,53 @@
+import { Outlet } from "react-router-dom";
+import logo from "./images/logo.png";
+import bicycle from "./images/icon-bicycle.png";
+import gym from "./images/icon-gym.png";
+import meditation from "./images/icon-meditation.png";
+import swim from "./images/icon-swim.png";
+
+export default function Layout() {
+  return (
+    <>
+      <div className="wrapper">
+        <div className="header-wrapper">
+          <header className="header-logo">
+            <img src={logo} alt="logo" className="logo" />
+          </header>
+          <nav className="navbar-h">
+            <ul>
+              <li>Accueil</li>
+              <li>Profil</li>
+              <li>Réglage</li>
+              <li>Communauté</li>
+            </ul>
+          </nav>
+          <nav className="navbar-v">
+            <ul>
+              <li>
+                <img
+                  src={meditation}
+                  alt="meditation-icon"
+                  className="nav-icon"
+                />
+              </li>
+              <li>
+                <img src={swim} alt="swim-icon" className="nav-icon" />
+              </li>
+              <li>
+                <img src={bicycle} alt="bicycle-icon" className="nav-icon" />
+              </li>
+              <li>
+                <img src={gym} alt="gym-icon" className="nav-icon" />
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        <Outlet />
+      </div>
+      <footer className="footer">
+        <p className="footer-text">Copiryght, SportSee 2020</p>
+      </footer>
+    </>
+  );
+}
