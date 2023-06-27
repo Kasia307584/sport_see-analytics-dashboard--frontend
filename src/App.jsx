6155e7f62fe7a9ebea5a7c1658947dbb340e60b3
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2023-06-27 18:48:16 +0200

    Data mock prep

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import * as React from "react";
 import { Routes, Route } from "react-router-dom";
 import PagesLayout from "./PagesLayout";
 // import User from "./User";
+import Test from "./test.jsx";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<PagesLayout />}>
         {/* <Route path=":userId" element={<User />} /> */}
+        <Route index element={<Test />} />
       </Route>
     </Routes>
   );
---
+++ src/test.jsx
@@ -0,0 +1,5 @@
+import data from "./dataMock.json";
+
+export default function Test() {
+  return console.log(data.USER_ACTIVITY);
+}
