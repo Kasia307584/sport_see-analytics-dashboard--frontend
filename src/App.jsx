455c7962f538728298f1478b60ab81202cfb73b5
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2023-06-30 10:20:59 +0200

    mock data user accessed

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@
 import * as React from "react";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./PagesLayout";
-import Dashboard from "./Dashboard";
-// import User from "./User";
+import User from "./User";
+// import Activity from "./Activity";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        {/* <Route path=":userId" element={<User />} /> */}
-        <Route path="user/:userId" element={<Dashboard />} />
+        <Route path="user/:userId" element={<User />} />
+        {/* <Route path="user/:userId/activity" element={<Activity />} /> */}
       </Route>
     </Routes>
   );
--- src/User.jsx
+++ src/User.jsx
@@ -1,18 +1,17 @@
 import { useParams } from "react-router-dom";
 import { getMainDataMock } from "./getDataMock";
 
-export default function Dashboard() {
+export default function User() {
   const { userId } = useParams();
+  const idNum = Number.parseInt(userId, 10);
 
   const data = getMainDataMock();
-  console.log(data);
 
-  const user = data.find((item) => item.id === userId);
-  console.log(user);
+  const user = data.find((item) => item.id === idNum);
 
   return (
     <div className="wrapper">
-      <h1>Bonjour {user}</h1>
+      <h1>Bonjour {user.userInfos.firstName}</h1>
     </div>
   );
 }
