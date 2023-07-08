2796ab71db1fa3b9165ca1fc2dd92324f8cd87b5
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2023-07-08 15:27:28 +0200

    PerformanceChart data formatted

--- src/components/performance/PerformanceChart.jsx
+++ src/components/performance/PerformanceChart.jsx
@@ -1,61 +1,22 @@
-// import { useParams } from "react-router-dom";
-// import { getPerformance } from "../../services/getDataMock";
+import { useParams } from "react-router-dom";
+import { getPerformance } from "../../services/getDataMock";
 import { Radar, RadarChart, PolarGrid, PolarAngleAxis } from "recharts";
 
-const data = [
-  {
-    subject: "Math",
-    A: 120,
-    B: 110,
-    fullMark: 150,
-  },
-  {
-    subject: "Chinese",
-    A: 98,
-    B: 130,
-    fullMark: 150,
-  },
-  {
-    subject: "English",
-    A: 86,
-    B: 130,
-    fullMark: 150,
-  },
-  {
-    subject: "Geography",
-    A: 99,
-    B: 100,
-    fullMark: 150,
-  },
-  {
-    subject: "Physics",
-    A: 85,
-    B: 90,
-    fullMark: 150,
-  },
-  {
-    subject: "History",
-    A: 65,
-    B: 85,
-    fullMark: 150,
-  },
-];
-
 export default function PerformanceChart() {
-  // const { userId } = useParams();
+  // const { performanceData } = props;
 
-  // const user = getPerformance(userId);
+  const { userId } = useParams();
 
-  // const chartSubject1 = user.kind[1];
-  // console.log(chartSubject1);
+  const performanceData = getPerformance(userId);
 
-  // const chartSubjects = Object.values(user.kind);
-  // console.log(chartSubjects);
+  const performanceDataFormatted = performanceData.data.map((item) => {
+    const performanceItem = {};
 
-  // const subjectValues = user.data.map((item) => {
-  //   return item.value;
-  // });
-  // console.log(subjectValues);
+    performanceItem.kind = performanceData.kind[item.kind];
+    performanceItem.value = item.value;
+
+    return performanceItem;
+  });
 
   return (
     <div className="performance-wrapper">
@@ -65,12 +26,17 @@ export default function PerformanceChart() {
         cx="50%"
         cy="50%"
         outerRadius="80%"
-        data={data}
+        data={performanceDataFormatted}
         style={{ background: "#000000" }}
       >
         <PolarGrid stroke="#FFFFFF" />
-        <PolarAngleAxis dataKey="subject" stroke="#FFFFFF" tickLine={false} />
-        <Radar dataKey="A" stroke="#FF0000" fill="#FF0000" fillOpacity={0.7} />
+        <PolarAngleAxis dataKey="kind" stroke="#FFFFFF" tickLine={false} />
+        <Radar
+          dataKey="value"
+          stroke="#FF0000"
+          fill="#FF0000"
+          fillOpacity={0.7}
+        />
       </RadarChart>
     </div>
   );
