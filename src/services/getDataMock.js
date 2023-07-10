400c9ce7ad6a6630d82deff6ba4f491c44187c11
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2023-07-10 12:17:52 +0200

    getDataMock update

--- src/services/getDataMock.js
+++ src/services/getDataMock.js
@@ -1,21 +1,17 @@
 import data from "./dataMock.json";
 
-const toNum = (id) => Number.parseInt(id, 10);
-
 export function getMainData(userId) {
-  return data.USER_MAIN_DATA.find((item) => item.id === toNum(userId));
+  return data.USER_MAIN_DATA.find((item) => item.id === userId);
 }
 
 export function getUserActivity(userId) {
-  return data.USER_ACTIVITY.find((item) => item.userId === toNum(userId));
+  return data.USER_ACTIVITY.find((item) => item.userId === userId);
 }
 export function getAverageSessions(userId) {
-  return data.USER_AVERAGE_SESSIONS.find(
-    (item) => item.userId === toNum(userId)
-  );
+  return data.USER_AVERAGE_SESSIONS.find((item) => item.userId === userId);
 }
 export function getPerformance(userId) {
-  return data.USER_PERFORMANCE.find((item) => item.userId === toNum(userId));
+  return data.USER_PERFORMANCE.find((item) => item.userId === userId);
 }
 
 export default async function getUserData(userId) {
