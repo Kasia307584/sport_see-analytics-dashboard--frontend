8a02a6c2a2ad89b263769083b1eb770baa45a46d
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2023-07-13 17:39:14 +0200

    DashboardView cleaned

--- src/pages/dashboard/DashboardView.jsx
+++ src/pages/dashboard/DashboardView.jsx
@@ -16,12 +16,9 @@ export default function Dashboard() {
 
   useEffect(() => {
     getFormattedData(Number.parseInt(userId, 10)).then((data) => {
-      console.log(data);
       setData(data);
     });
   }, [userId]);
-
-  console.log(userData);
 
   return (
     <main className={styles["main-content"]}>
@@ -36,10 +33,3 @@ export default function Dashboard() {
     </main>
   );
 }
-
-// je fais mon fetch dans ce composant pour passer les donnees comme props a tous les composants
-// tu fait ici useEffect et appelle les 4 functions de fetch et le stocker dans le state
-// par contre les donnees ne sont pas celles que j'ai besion
-// useEffect va appler la fonction qui modifie les donnees et cette fonction va appeller les 4 fonctions
-// puis les state va passer les donnees parametrees vers mes composants avec props
-// dans la function qui modifie les donnees je vais preciser si je suis en version moquee ou pas
